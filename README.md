e94c27c396665868b2c348785142ffb662bcd31c
sedith <martin.jacquet@posteo.net>
2026-05-09 19:46:44 +0200

    comment out acro-related stuff and handle recursive fig folder

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ Copy the `merge_latex.py` script into your latex root folder, set the input/outp
 
 The features are:
 1. Every `\input` will be expanded to the content of the corresponding file.
-1. The figures are copied to the output folder, removing the subfolder structure. The extension is replaced by `.pdf`, make sure to have a pdf version of each figures!
-1. The `\bibliography{whatever}` is expanded to the content of the pre-generated `.bbl`. Make sure to have it in the root folder.
-1. Commented lines are ignored.
+2. The figures are converted to jpg and copied to the output folder, removing the subfolder structure.
+3. The `\bibliography{whatever}` is expanded to the content of the pre-generated `.bbl`. Make sure to have it in the root folder.
+4. Commented lines are ignored. (not handle `\iffalse ... \fi`)
 
 ## Acro package
 
 The script expands the acro package macros manually, in case the package is not working with online compilers.
-The `\DeclareAcronym` should all be in a single separate file (you should put your `\usepackage{acro}` therein, since this file is not expanded into the final `.tex`).
+The `\DeclareAcronym` should all be in a single separate file named `...glossary.tex` (you should put your `\usepackage{acro}` therein, since this file is not expanded into the final `.tex`).
 Once the corresponding `\input` is encountered, the file is parsed and a glossary dict is created.
 The subsequent `\ac*` commands will be expanded in the same way the acro package does.
 The following features are handled so far:
@@ -27,3 +27,13 @@ The following features are handled so far:
   * `short-plural-form`, `long-plural-form`
 
 Moreover, if you declare shortcuts for acronyms (e.g., `\newcommand{\fov}{\ac{FOV}\xspace}`), they will also be expanded in the output latex file.
+
+## TODO handle SI package
+
+Replacement of `\usepackage{siunitx}`:
+```latex
+\newcommand{\num}[1]{#1}
+\newcommand{\unit}[1]{\ensuremath{\mathrm{#1}}}
+\newcommand{\si}[1]{\ensuremath{\mathrm{#1}}}
+\newcommand{\SI}[2]{\ensuremath{#1\,\mathrm{#2}}}
+```
--- merge_latex.py
+++ merge_latex.py
@@ -1,13 +1,15 @@
-import os
+from pathlib import Path
 import shutil
 from PIL import Image
+from pdf2image import convert_from_path
 
 
 out_folder = 'submission_material'
-fig_folder = 'figures'
+fig_folder = 'figs'
 latex_main = 'main.tex'
 glossary = {}
 shortcuts = {}
+figure_paths = []
 
 
 class GlossaryEntry:
@@ -46,42 +48,42 @@ class GlossaryEntry:
         return self.long + plural * 's'
 
 
-def glossary_dict(path):
-    global glossary, shortcuts
+# def glossary_dict(path: str):
+#     global glossary, shortcuts
 
-    with open(path, 'r') as infile:
-        lines = infile.readlines()
+#     with Path(path).open('r') as infile:
+#         lines = infile.readlines()
 
-    for l in lines:
-        if '\\DeclareAcronym' in l:
-            key = l.split('{')[1].split('}')[0]
-            glossary[key] = GlossaryEntry(key)
-        elif 'short = ' in l:
-            glossary[key].short = l.split(' = ')[1].split(',')[0]
-        elif 'long = ' in l:
-            glossary[key].long = l.split(' = ')[1].split(',')[0].split('\n')[0]
-        elif 'first-style = short' in l:
-            glossary[key].first = 'short'
-        elif 'first-style = long' in l:
-            glossary[key].first = 'long'
-        elif 'long-plural-form' in l:
-            glossary[key].long_plural = l.split(' = ')[1].split(',')[0].split('\n')[0]
-        elif 'short-plural-form' in l:
-            glossary[key].short_plural = l.split(' = ')[1].split(',')[0].split('\n')[0]
-        elif '\\newcommand{' in l:
-            key = l.split('{')[1].split('}')[0]
-            val = (l.split('{')[2] + '{' + l.split('{')[3]).split('}')[0] + '}'
-            shortcuts[key] = val
+#     for l in lines:
+#         if '\\DeclareAcronym' in l:
+#             key = l.split('{')[1].split('}')[0]
+#             glossary[key] = GlossaryEntry(key)
+#         elif 'short = ' in l:
+#             glossary[key].short = l.split(' = ')[1].split(',')[0]
+#         elif 'long = ' in l:
+#             glossary[key].long = l.split(' = ')[1].split(',')[0].split('\n')[0]
+#         elif 'first-style = short' in l:
+#             glossary[key].first = 'short'
+#         elif 'first-style = long' in l:
+#             glossary[key].first = 'long'
+#         elif 'long-plural-form' in l:
+#             glossary[key].long_plural = l.split(' = ')[1].split(',')[0].split('\n')[0]
+#         elif 'short-plural-form' in l:
+#             glossary[key].short_plural = l.split(' = ')[1].split(',')[0].split('\n')[0]
+#         elif '\\newcommand{' in l:
+#             key = l.split('{')[1].split('}')[0]
+#             val = (l.split('{')[2] + '{' + l.split('{')[3]).split('}')[0] + '}'
+#             shortcuts[key] = val
 
-    for key in glossary.keys():
-        glossary[key].short = expand_line(glossary[key].short)
-        glossary[key].long = expand_line(glossary[key].long)
+#     for key in glossary.keys():
+#         glossary[key].short = expand_line(glossary[key].short)
+#         glossary[key].long = expand_line(glossary[key].long)
 
-    for key in glossary.keys():
-        glossary[key].used = False
+#     for key in glossary.keys():
+#         glossary[key].used = False
 
 
-def expand_line(l):
+def expand_line(l: str):
     global glossary, shortcuts
 
     if len(l) == 1:
@@ -89,103 +91,111 @@ def expand_line(l):
     if not l.split() or l.split()[0].startswith('%'):
         return ''
 
-    if '\\acresetall' in l:
-        for key in glossary.keys():
-            glossary[key].used = False
-        return ''
+    # print(l)
 
-    for key in shortcuts.keys():
-        if key in l.replace('-', ' ').replace(',', ' ').replace('.', ' ').replace('~', ' ').replace('\'', ' ').replace('(', ' ').replace(')', ' ').split():
-            l = l.replace(key, shortcuts[key])
+    # if '\\acresetall' in l:
+    #     for key in glossary.keys():
+    #         glossary[key].used = False
+    #     return ''
 
-    nb_ac = l.count('\\ac')
-    for i in range(nb_ac):
-        start = l.find('\\ac')
-        command = l[start:].split('{')[0]
-        key = l[start+len(command)+1:].split('}')[0]
-        if command == '\\ac':
-            expanded = glossary[key].s()
-        elif command == '\\acf':
-            expanded = glossary[key].s_first()
-        elif command == '\\acs':
-            expanded = glossary[key].s_short()
-        elif command == '\\acl':
-            expanded = glossary[key].s_long()
-        elif command == '\\acp':
-            expanded = glossary[key].s(plural=True)
-        elif command in ['\\acfp', '\\acpf']:
-            expanded = glossary[key].s_first(plural=True)
-        elif command in ['\\acsp', '\\acps']:
-            expanded = glossary[key].s_short(plural=True)
-        elif command in ['\\aclp', '\\acpl']:
-            expanded = glossary[key].s_long(plural=True)
-        l = l.replace(command +'{' + key + '}', expanded)
+    # for key in shortcuts.keys():
+    #     if key in l.replace('-', ' ').replace(',', ' ').replace('.', ' ').replace('~', ' ').replace('\'', ' ').replace('(', ' ').replace(')', ' ').split():
+    #         l = l.replace(key, shortcuts[key])
+
+    # nb_ac = l.count('\\ac')
+    # for i in range(nb_ac):
+    #     start = l.find('\\ac')
+    #     command = l[start:].split('{')[0]
+    #     key = l[start+len(command)+1:].split('}')[0]
+    #     if command == '\\ac':
+    #         expanded = glossary[key].s()
+    #     elif command == '\\acf':
+    #         expanded = glossary[key].s_first()
+    #     elif command == '\\acs':
+    #         expanded = glossary[key].s_short()
+    #     elif command == '\\acl':
+    #         expanded = glossary[key].s_long()
+    #     elif command == '\\acp':
+    #         expanded = glossary[key].s(plural=True)
+    #     elif command in ['\\acfp', '\\acpf']:
+    #         expanded = glossary[key].s_first(plural=True)
+    #     elif command in ['\\acsp', '\\acps']:
+    #         expanded = glossary[key].s_short(plural=True)
+    #     elif command in ['\\aclp', '\\acpl']:
+    #         expanded = glossary[key].s_long(plural=True)
+        # l = l.replace(command +'{' + key + '}', expanded)
     return l
 
 
-def expand_latex_rec(path):
-    global glossary
+def expand_latex_rec(path: str):
+    global glossary, figure_paths
     output_lines = ''
 
-    with open(path, 'r') as infile:
+    with Path(path).open('r') as infile:
         lines = infile.readlines()
 
     for l in lines:
-        if '\\input{' in l:
-            input_path = l.split('{')[1].split('}')[0]
-            if not input_path.endswith('.tex'):
-                input_path += '.tex'
-            if 'glossary' in input_path:
-                glossary_dict(input_path)
-            else:
-                output_lines += expand_latex_rec(input_path) + '\n'
-        elif '\\includegraphics' in l:
-            figure_path = l.split('{')[1].split('}')[0]
-            output_lines += l.replace(figure_path, figure_path.split('/')[-1].split('.')[0] + '.jpg')
-        elif '\\bibliography{' in l:
+        if len(l) == 1:
+            output_lines += l
+        elif not l.split() or l.split()[0].startswith('%'):
+            continue
+        elif r'\input{' in l:
+            input_path = Path(l.split('{')[1].split('}')[0])
+            if input_path.suffix != '.tex':
+                input_path = input_path.with_suffix('.tex')
+            output_lines += expand_latex_rec(input_path) + '\n'
+        elif r'\includegraphics' in l:
+            figure_path = Path(l.split('{')[1].split('}')[0])
+            figure_paths.append(figure_path)
+            output_lines += l.replace(figure_path.as_posix(), figure_path.stem + '.jpg')
+        elif r'\bibliography{' in l:
             output_lines += expand_latex_rec('main.bbl') + '\n'
         else:
-            output_lines += expand_line(l)
+            output_lines += l
     return output_lines
 
 
 def figures_jpg(dpi=200):
+    global figure_paths 
+
     ## convert image figures to jpg
-    imgs = [os.path.join(fig_folder, f) for f in os.listdir(fig_folder) if f.endswith(('.png', 'jpg', 'jpeg'))]
-    for f_img in imgs:
-        img = Image.open(f_img).convert("RGB")
-        img.save(os.path.join(
-            out_folder,
-            os.path.splitext(f_img)[0] + '.jpg'
-        ))
+    seen = set()
+    for f_img in figure_paths:
+        print(f_img)
+        f_img = Path(f_img)
 
-    ## convert pdfs figures to jpg
-    pdfs = [os.path.join(fig_folder, f) for f in os.listdir(fig_folder) if f.endswith('.pdf')]
-    for f_img in pdfs:
-        img = convert_from_path(f_img, dpi=dpi)[0]
-        img.save(os.path.join(
-            out_folder,
-            os.path.splitext(f_img)[0] + '.jpg'
-        ))
+        if f_img in seen:
+            continue
+        seen.add(f_img)
 
-    ## TODO handle svg?
+        out_path = out_folder / f'{f_img.stem}.jpg'
 
+        if f_img.suffix.lower() in ['.png', '.jpg', '.jpeg']:
+            img = Image.open(f_img).convert("RGB")
+            img.save(out_path)
+        elif f_img.suffix.lower() == '.pdf':
+            img = convert_from_path(f_img, dpi=dpi)[0]
+            img.save(out_path)
+        else:
+            print(f'WARNING: unsupported figure format: {f_img}')
 
 if __name__ == '__main__':
-    os.makedirs(out_folder, exist_ok=True)
+    out_folder = Path(out_folder)
+    fig_folder = Path(fig_folder)
 
-    ## convert figures to jpg
-    figures_jpg(dpi=300)
+    out_folder.mkdir(exist_ok=True)
 
     ## copy bst and cls
-    bst_cls = [f for f in os.listdir('.') if f.endswith('.bst') or f.endswith('.cls')]
+    bst_cls = [f for f in Path('.').iterdir() if f.suffix in ('.bst', '.cls')]
     for f in bst_cls:
         shutil.copy2(f, out_folder)
 
     ## process latex_main
     output_lines = expand_latex_rec(latex_main)
-    # output_lines = output_lines.replace(r'\revisionadd', '')
+
+    ## convert figures to jpg
+    # figures_jpg(dpi=300)
 
     ## write
-    with open(os.path.join(out_folder, latex_main), 'w') as outfile:
+    with (out_folder / latex_main).open('w') as outfile:
         outfile.write(output_lines)
